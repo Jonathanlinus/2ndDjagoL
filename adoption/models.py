019acc9c6987f02fd91da68c5cb2196ea7a63d74
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-10 17:29:57 +0330

    first migration donea

--- adoption/models.py
+++ adoption/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Pet(models.Model):
-	SEX_CHOICES=[('M','Male'),('F'),('Female')]
+	SEX_CHOICES=[('M','Male'),('F','Female')]
 	
 	name = models.CharField ( max_length=100 )
 	submitter = models.CharField ( max_length=100 )
 	species = models.CharField ( max_length=30 )
 	breed = models.CharField ( max_length=30 )
 	description = models.TextField()
-	sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+	sex = models.CharField(choices=SEX_CHOICES, blank=True, max_length=1)
 	submission_date = models.DateField()
 	age = models.IntegerField(null=True)
 	vaccinations = models.ManyToManyField('Vaccine', blank=True)
